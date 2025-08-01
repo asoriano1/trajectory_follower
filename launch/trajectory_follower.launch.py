67e77f2cf6807e4cbcee1ef83422d78ae6fe38a5
albertmg2023 <113520317+albertmg2023@users.noreply.github.com>
2025-08-01 22:35:43 +0200

    waypoints file parametrizedand added more examples

--- launch/trajectory_follower.launch.py
+++ launch/trajectory_follower.launch.py
@@ -1,24 +1,31 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
-    # Encuentra la ruta absoluta al YAML de waypoints
-    config_path = os.path.join(
-        get_package_share_directory('trajectory_follower'),
-        'config',
-        'waypoints.yaml'
+    config_dir = [get_package_share_directory('trajectory_follower'), 'config']
+
+    yaml_filename_arg = DeclareLaunchArgument(
+        'yaml_filename',
+        default_value='grid_map_waypoints.yaml',
+        description='Nombre del archivo YAML con los waypoints'
     )
 
-    return LaunchDescription([
-        Node(
-            package='trajectory_follower',
-            executable='trajectory_follower',
-            name='trajectory_follower',
-            output='screen',
-            parameters=[{'use_sim_time': True}],
-            arguments=[config_path]
-        )
+    yaml_path = PathJoinSubstitution([
+        *config_dir,
+        LaunchConfiguration('yaml_filename')
     ])
 
+    return LaunchDescription([
+        yaml_filename_arg,
+        Node(
+            package='trajectory_follower',
+            executable='trajectory_follower',  # O solo 'trajectory_follower' si tienes el ejecutable compilado
+            name='trajectory_follower',
+            output='screen',
+            arguments=[yaml_path],  # <= Tu nodo espera el YAML como argumento
+            parameters=[{'use_sim_time': True}]
+        )
+    ])
